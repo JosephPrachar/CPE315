e4d0b016593d62993b279ea6e8289da6b5dbae1c
Joseph Prachar <josephprachar@comcast.net>
2016-10-12 23:02:03 -0700

    Lab 2 done

---
+++ lab2/word.asm
@@ -0,0 +1,31 @@
+# lab2.asm
+# This MIPS assembly program will calculate the sum of a table of words
+# then a table of bytes
+
+.data
+
+table:     .word    -1, 2, 3, -10, 20, 30, -100, 200, 300
+table_len: .byte    9
+
+.text
+# Sum the table of words
+#   $t0 - Length of table
+#   $t1 - Ptr to table1
+#   $t2 - Counter
+#   $t3 - 
+#   $t4 - Next value to add
+#   $t5 - Value of sum
+main:  lb $t0, table_len        # Load length byte into t0
+       la $t1, table            # Load address of set into t1
+Loop1: lw $t4, 0($t1)            # Load value at address t1 into t4
+       add $t5, $t4, $t5         # Add value of t4 to running sum (t5)
+       add $t1, 0x4              # Increment pointer by sizeof(word)=4
+       add $t2, 0x1              # Increment counter
+       blt $t2, $t0, Loop1       # Goto Loop1 if counter < t0 (length)
+       li $v0, 0x1               # Set syscall to output
+       move $a0, $t5             # Set number to output with syscall
+       syscall
+
+
+li $v0, 0xA      # Set syscall to quit
+syscall
